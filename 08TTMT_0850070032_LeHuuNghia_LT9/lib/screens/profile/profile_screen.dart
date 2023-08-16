8d2a4f53e252f0ee3051fad2e543f31b50aab9a1
nghia <len265734@gmail.com>
2023-08-16 22:46:48 +0700

    LT9

---
+++ 08TTMT_0850070032_LeHuuNghia_LT9/lib/screens/profile/profile_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:buoi9/components/coustom_bottom_nav_bar.dart';
+import 'package:buoi9/enums.dart';
+
+import 'components/body.dart';
+
+class ProfileScreen extends StatelessWidget {
+  static String routeName = "/profile";
+
+  const ProfileScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      body: const Body(),
+      // ignore: prefer_const_constructors
+      bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.profile),
+    );
+  }
+}
